8c1daf0afc69cf5f766ee1e4944b702eb698c7a2
ISADORA CABRAL ORDONHO <54763965867@info.local>
2025-11-13 17:28:02 -0300

    Componentizando os produtos e o carrosel

--- src/types/Bolo.tsx
+++ src/types/Bolo.tsx
@@ -1,5 +1,9 @@
 export interface Bolo {
     nome: string;  
+    descricao: string[];
     id: string | undefined;
     categorias: string [], 
+    imagens: string[];
+    preco: number;
+    peso: number | null
 }
---
+++ src/types/CardProdutoProps.tsx
@@ -0,0 +1,9 @@
+export interface CardProdutoProps {
+    nome: string;
+    descricao: string;
+    preco: number;
+    imagem: string;
+    id?: string;
+    peso: number | null;
+
+}
